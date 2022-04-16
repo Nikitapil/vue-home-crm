9e62a60f3f36f58cdd9afa016c71745cba2f1f39
Nikitapil <nikitapiliavets@MacBook-Pro-Nikita.local>
2022-04-16 18:11:35 +0300

    deploy

---
+++ precache-manifest.31e4ad78f6451759c7393450560cf345.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f04eb2d723d934044b",
+    "url": "/vue-home-crm/css/app.4cd6f013.css"
+  },
+  {
+    "revision": "5f9b72222749cae14ec4",
+    "url": "/vue-home-crm/css/chunk-2d8325e2.a2f7ed3c.css"
+  },
+  {
+    "revision": "97601fdd6e9c22ad0c33",
+    "url": "/vue-home-crm/css/chunk-53a36446.dba4372e.css"
+  },
+  {
+    "revision": "e62eb675d30a52e7e7b3",
+    "url": "/vue-home-crm/css/chunk-82b15f34.398d2648.css"
+  },
+  {
+    "revision": "8ebde81849a60d7298db",
+    "url": "/vue-home-crm/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "b841d6f0b42d7a996bea08a4d9cb55fe",
+    "url": "/vue-home-crm/index.html"
+  },
+  {
+    "revision": "a2f04eb2d723d934044b",
+    "url": "/vue-home-crm/js/app.57e5fad4.js"
+  },
+  {
+    "revision": "498049144afde842ac8e",
+    "url": "/vue-home-crm/js/chunk-0fbd6bac.f71775e5.js"
+  },
+  {
+    "revision": "360cbace9450cb3f5d0f",
+    "url": "/vue-home-crm/js/chunk-2d0d7699.625aeb70.js"
+  },
+  {
+    "revision": "5f9b72222749cae14ec4",
+    "url": "/vue-home-crm/js/chunk-2d8325e2.e8efac3e.js"
+  },
+  {
+    "revision": "0479fc8996c1eb969992",
+    "url": "/vue-home-crm/js/chunk-458a41cc.08ecd7da.js"
+  },
+  {
+    "revision": "97601fdd6e9c22ad0c33",
+    "url": "/vue-home-crm/js/chunk-53a36446.3d32445e.js"
+  },
+  {
+    "revision": "cbcff72003c329e4f9a4",
+    "url": "/vue-home-crm/js/chunk-5b3e162e.05989c73.js"
+  },
+  {
+    "revision": "d8d0f29f85a0bb8ebd50",
+    "url": "/vue-home-crm/js/chunk-7f4c16aa.f26bd420.js"
+  },
+  {
+    "revision": "e62eb675d30a52e7e7b3",
+    "url": "/vue-home-crm/js/chunk-82b15f34.3196e546.js"
+  },
+  {
+    "revision": "8ebde81849a60d7298db",
+    "url": "/vue-home-crm/js/chunk-vendors.adc4fb16.js"
+  },
+  {
+    "revision": "6a8d2c9e18169fece64b94593566de0d",
+    "url": "/vue-home-crm/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-home-crm/robots.txt"
+  }
+]);
